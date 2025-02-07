ca1a112f722c2256f1b37d86828719a83350ee01
pkhaneff <nguyenminhlong1918@gmail.com>
2025-02-07 11:41:35 +0700

    test AI code review

--- .ai/io/nerdythings/github_reviewer.py
+++ .ai/io/nerdythings/github_reviewer.py
@@ -55,7 +55,7 @@ def main():
         Log.print_green(f"Asking AI. Content Len:{len(file_content)} Diff Len: {len(file_diffs)}")
         response = ai.ai_request_diffs(code=file_content, diffs=file_diffs)
 
-        with open("log.txt", "w", encoding="utf-8") as log_file:
+        with open("output.txt", "w", encoding="utf-8") as log_file:
             log_file.write(f"{separator}{file_content}{separator}{file_diffs}{separator}{response}{separator}")
 
 
