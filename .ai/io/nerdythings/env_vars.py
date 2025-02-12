c0970e83900fcb4f9521b764a4837582b3959f6d
pkhaneff <nguyenminhlong1918@gmail.com>
2025-02-12 09:57:20 +0700

    fix new comment for PR

--- .ai/io/nerdythings/env_vars.py
+++ .ai/io/nerdythings/env_vars.py
@@ -3,7 +3,8 @@ import requests
 from log import Log
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path=dotenv_path)
 
 class EnvVars:
     def __init__(self):
