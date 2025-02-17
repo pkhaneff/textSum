fe1b8640575688ff9bbe321649626566798ba9fe
pkhaneff <nguyenminhlong1918@gmail.com>
2025-02-17 15:35:00 +0700

    final-v.5 test 4

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def clean_text(text: str) -> str:
 
 def summarize_dialogue(dialogue: str) -> str:
     dialogue = clean_text(dialogue)
-    inputs = tokenizer(dialogue, return_tensors="pt", padding="max_length", max_length=512)
+    inputs = tokenizer(dialogue, return_tensors="pt", truncation=True, padding="max_length", max_length=512)
 
     outputs = model.generate(
         inputs["input_ids"],
@@ -39,7 +39,7 @@ def summarize_dialogue(dialogue: str) -> str:
         early_stopping=True
     )
     summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return result
+    return summary
 
 @app.post('/summarize/')
 async def summarize(dialogue_input: DialogueInput):
